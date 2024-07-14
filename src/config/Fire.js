ccdde1e9ae75fa4a94328289599e5e6b2723a8b0
Zahir Hadi Athallah <89567774+Jahirrrr@users.noreply.github.com>
2024-07-14 14:35:17 +0700

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -4,14 +4,14 @@ import 'firebase/storage';
 import 'firebase/auth';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBjlzmxfNyInpjo257xMFUmYwr6aCTej14",
-  authDomain: "crackin-beta.firebaseapp.com",
-  databaseURL: "https://crackin-beta-default-rtdb.firebaseio.com",
-  projectId: "crackin-beta",
-  storageBucket: "crackin-beta.appspot.com",
-  messagingSenderId: "663707823384",
-  appId: "1:663707823384:web:5ba2a489f9590f610b426d",
-  measurementId: "G-KLG77QDEDH"
+  apiKey: "AIzaSyBpC_J5GzPrkKP54wLoSXWLAN9N66-Hkg0",
+  authDomain: "itclub21.firebaseapp.com",
+  databaseURL: "https://itclub21-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "itclub21",
+  storageBucket: "itclub21.appspot.com",
+  messagingSenderId: "622811807432",
+  appId: "1:622811807432:web:c1e4ae3e31a4a44a95affd",
+  measurementId: "G-PHQBH4KEVJ"
 };
   const fire = firebase.initializeApp(firebaseConfig);
 
